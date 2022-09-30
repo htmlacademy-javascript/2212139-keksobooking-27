6188b256030850907ff333bd880d2403b91d12e2
Alexey Makeev <makeevhome@mail.ru>
2022-09-30 19:00:33 +0300

    получил функции от кекса

--- js/main.js
+++ js/main.js
@@ -1,34 +1,59 @@
-function randomIntFromInterval(min, max) {
-  // Результат: целое число из диапазона "от...до"
-  // min и max включены в диапазон
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-  if (min < 0 || max < 0) {
+function getRandomPositiveInteger(a, b) {
+  // Если переданы отрицительные числа, возвращаем NaN
+  if (a < 0 || b < 0) {
     return NaN;
   }
 
-  if (min >= max) {
-    return randomIntFromInterval(max, min);
-  }
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
 
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  // После нам нужно убедиться, что пользователь не передал дробные значения,
+  // для этого на всякий пожарный случай нижнюю границу диапазона
+  // мы округляем к ближайшему большему целому с помощью Math.ceil,
+  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower + 1) + lower;
+  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
+
+  // И в конце с помощью метода Math.floor мы округляем полученный результат,
+  // потому что Math.random() генерирует только дробные числа и ноль.
+  return Math.floor(result);
 }
 
-function randomFloatFromInterval(min, max, precision) {
-  // Результат: число с плавающей точкой из диапазона "от...до"
-  //с указанным "количеством знаков после запятой"
+// Функция взята из интернета и доработана
+// Источник - https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore#_random
 
-  if (min < 0 || max < 0) {
+function getRandomPositiveFloat(a, b, digits = 1) {
+  // Если переданы отрицительные числа, возвращаем NaN
+  if (a < 0 || b < 0 || digits < 0) {
     return NaN;
   }
 
-  if (min >= max) {
-    return randomIntFromInterval(max, min, precision);
-  }
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max
+  const lower = Math.min(a, b);
+  const upper = Math.max(a, b);
 
-  return +(Math.random() * (max - min) + min).toFixed(precision);
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+  const result = Math.random() * (upper - lower) + lower;
+
+  // И в конце с помощью метода toFixed любого числа в JavaScript
+  // указать требуемое количество знаков после точки.
+  // Метод возвращает строку, поэтому с помощью унарного плюса превращаем её в число
+  return +result.toFixed(digits);
 }
 
-randomIntFromInterval(1, 2);
-randomFloatFromInterval(1.1, 1.3, 2);
+getRandomPositiveFloat(0, 1.1, 3);
+getRandomPositiveInteger(0, 1);
