59db9705a732e7d10e39c933fbc3a73bbcf3c38d
Alexey Makeev <makeevhome@mail.ru>
2022-10-11 17:21:33 +0300

    11

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { similarListFragment } from './popup.js';
 // import { modePage } from './mode.js';
+import './validate.js';
 
 document.querySelector('#map-canvas').appendChild(similarListFragment);
 
--- js/validate.js
+++ js/validate.js
@@ -1,37 +1,43 @@
-const advertForm = document.querySelector('.ad-form'); // для добавления на форму стороннего валидатора
 
-const pristine = new Pristine(advertForm, { // создаем валидатор
+// для добавления на форму стороннего валидатора
+const advertForm = document.querySelector('.ad-form');
+
+// создаем экземпляр валидатора
+const pristine = new Pristine(advertForm, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form__element--invalid',
   successClass: 'ad-form__element--valid',
-  errorTextParent: 'form__item',
-  errorTextTag: 'span',
+  errorTextParent: 'ad-form__label',
+  errorTextTag: 'fieldset',
   errorTextClass: 'error__message'
 });
 
+// вынесенная функция проверки "Заголовок объявления"
 function validateTitle(value) {
   return value.length >= 30 && value.length <= 100;
 }
 
+// добавление поля "Заголовок объявления" для проверки валидатором
 pristine.addValidator(advertForm.querySelector('#title'),
   validateTitle, 'От 30 до 100 символов');
 
+// добавление проверки поля "Цена за ночь"
 const priceField = document.querySelector('#price');
-
 pristine.addValidator(priceField, validatePrice, 'Не больше 100000');
-
 function validatePrice(value) {
   return value < 100000;
 }
 
+// добавление проверки полей "Количесво комнат" и "Количество мест"
 const roomsField = document.querySelector('[name="rooms"]');
 const capacityField = document.querySelector('[name="capacity"]');
 
+// объект для создания логики проверки комнат-мест
 const roomsOption = {
-  '1': ['1'],
-  '2': ['1', '2'],
-  '3': ['1', '2', '3'],
-  '100': ['0']
+  1: ['1'],
+  2: ['1', '2'],
+  3: ['1', '2', '3'],
+  100: ['0']
 };
 
 function validateRoomsAndCapacity() {
@@ -41,7 +47,7 @@ function validateRoomsAndCapacity() {
 pristine.addValidator(roomsField, validateRoomsAndCapacity);
 pristine.addValidator(capacityField, validateRoomsAndCapacity);
 
-
+// событие, по котору происходит проверка формы
 advertForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
