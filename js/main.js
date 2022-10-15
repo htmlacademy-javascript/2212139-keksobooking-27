435727703cb00c2f6848e2a99b80295b5fecae3d
Alexey Makeev <makeevhome@mail.ru>
2022-10-15 23:40:36 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
 import { validate } from './validate.js';
-import './map.js';
+import { createMarker } from './map.js';
 import { modePage } from './mode.js';
+import { createOffers } from './data.js';
 
 
 const button = document.querySelector('.ad-form__submit');
-
 button.addEventListener('click', () => validate());
-
 modePage(false);
 
-
+const offers = Array.from(createOffers);
+offers.forEach((offer) => createMarker(offer));
