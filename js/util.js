5f623f2625959b97a8945db0e400e6ce146f8ef5
Alexey Makeev <makeevhome@mail.ru>
2022-11-03 19:13:06 +0300

    Удалил лишнюю функцию

--- js/util.js
+++ js/util.js
@@ -9,15 +9,4 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-function throttle(callback, delayBetweenFrames) {
-  let lastTime = 0;
-  return (...rest) => {
-    const now = new Date();
-    if (now - lastTime >= delayBetweenFrames) {
-      callback.apply(this, rest);
-      lastTime = now;
-    }
-  };
-}
-
-export { isNumberInRange, debounce, throttle };
+export { isNumberInRange, debounce };
